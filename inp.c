c4093a9773892ba11f0b4ea268226ef53a5716ec
will-the-creator <hoopa05.cascade@icloud.com>
2025-11-22 12:27:53 -0700

    hello

---
+++ inp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char buffer_1[100];
+    char buffer_2[100];
+    printf("Enter pwd >:");
+    fgets(buffer_1, sizeof(buffer_1), stdin);
+    printf("Enter pwd again >:");
+    fgets(buffer_2, sizeof(buffer_2), stdin);
+    if (buffer_1 == buffer_2) {
+        printf("there the same");
+    } else {
+        printf("not the same");
+    }
+}
