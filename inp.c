980f6cd9c655a17b70d1c5302db64a18235ace53
will-the-creator <hoopa05.cascade@icloud.com>
2025-11-22 13:03:01 -0700

    add git

--- inp.c
+++ inp.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char buffer_1[100];
     char buffer_2[100];
     printf("Enter pwd >:");
     fgets(buffer_1, sizeof(buffer_1), stdin);
+    
     printf("Enter pwd again >:");
     fgets(buffer_2, sizeof(buffer_2), stdin);
-    if (buffer_1 == buffer_2) {
+    
+    if (strcmp(buffer_1, buffer_2) == 0) {
         printf("there the same");
     } else {
         printf("not the same");
