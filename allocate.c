37c44ba8e3108e03982d1807efe2e5e71e9e21e7
will-or3 <william@proton.me>
2026-07-26 23:29:18 -0600

    cli

--- allocate.c
+++ allocate.c
@@ -1,23 +1,51 @@
 // gcc -Wall -Wextra -o fake_usb allocate.c
+// ./fake_usb create fake_usb.img 5G
 
 #define _FILE_OFFSET_BITS 64
 
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    int fd = open("fake_usb.img", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+off_t parse_size(const char *size_str) {
+    char *endptr;
+    long long value = strtoll(size_str, &endptr, 10);
+
+    if (endptr == size_str || value <= 0) {
+        return 0; 
+    }
+
+    switch (*endptr) {
+        case 'G': case 'g':
+            return (off_t)value * 1024 * 1024 * 1024;
+        case 'M': case 'm':
+            return (off_t)value * 1024 * 1024;
+        case 'K': case 'k':
+            return (off_t)value * 1024;
+        case '\0':
+            return (off_t)value;
+        default:
+            return 0;
+    }
+}
+
+
+
+int allocate(const char *file_name, off_t size){
+    int fd = open(
+        file_name, 
+        O_RDWR | O_CREAT | O_TRUNC, 
+        0644);
 
     if (fd == -1) {
         perror("couldnt open");
         return 1;
     }
-
-    off_t size = 5LL * 1024 * 1024 * 1024; // 5gb
-    int err = posix_fallocate(fd, 0, size);
-     if (err) {
-        fprintf(stderr, "posix_fallocate failed: %d\n", err);
+    
+    if (posix_fallocate(fd, 0, size) != 0) {
+        perror("posix_fallocate");
         close(fd);
         return 1;
     }
@@ -25,6 +53,19 @@ int main(){
     close(fd);
 
     printf("Created fake_usb.img (%lld bytes)\n", (long long)size);
-    
+
     return 0;
+}
+
+
+int main(int argc, char *argv[]){
+    if (argc != 4 || strcmp(argv[1], "create") != 0) {
+        fprintf(stderr, "Usage: %s create <filename> <size[K|M|G]>\n", argv[0]);
+        return 1;
+    }
+
+    const char *file_name = argv[2];
+    off_t size = parse_size(argv[3]);
+
+    return allocate(file_name, size);
 }
